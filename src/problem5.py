8cee7422cdf6a3edc7da9aaca3727d8d2bf236f2
wenningr <wenningr@rose-hulman.edu>
2019-01-31 20:41:42 -0500

    DONE

--- src/problem5.py
+++ src/problem5.py
@@ -3,13 +3,13 @@ Exam 3, problem 5.
 
 Authors: Vibha Alangar, Aaron Wilkin, David Mutchler, Dave Fisher, 
          Matt Boutell, Amanda Stouder, their colleagues and 
-         PUT_YOUR_NAME_HERE.  January 2019.
+         Greg Wenning.  January 2019.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # DONE 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
-
+import math
 
 def main():
     """ Calls the   TEST   functions in this module. """
@@ -17,7 +17,7 @@ def main():
 
 
 ###############################################################################
-# TODO: 2.  READ the doc-string for the   is_prime   function defined below.
+# DONE: 2.  READ the doc-string for the   is_prime   function defined below.
 # It is the same as you have seen before.
 # After you UNDERSTAND the doc-string (JUST the doc-string, NOT the code),
 # ASKING QUESTIONS AS NEEDED, change the above _TODO_ to DONE.
@@ -145,9 +145,19 @@ def problem5(seq_of_seq):
       :rtype: (list of int) | int
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+
+    new_list = []
+    for k in range(len(seq_of_seq)):
+        high_number = -math.inf
+        for j in range(len(seq_of_seq[k])):
+            if seq_of_seq[k][j] > high_number:
+                high_number = seq_of_seq[k][j]
+        if high_number != -math.inf:
+            new_list = new_list + [high_number]
+    return new_list
 
 
 ###############################################################################
